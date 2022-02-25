9cf363ccea6ee7f2e121baf385432b5d14c7c53a
nikhil18-9192 <nikhilkorane08@gmail.com>
2022-02-25 12:13:27 +0530

    create and update student and classroom table done

--- institute/views.py
+++ institute/views.py
@@ -7,12 +7,54 @@ from django.db.models import Max, Min
 # Create your views here.
 
 
+def create_student(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode("utf-8"))
+        selected_classroom = Classroom.objects.get(id=data['classroom_id'])
+        name = data['name']
+        address = data['address']
+        percentage = data['percentage']
+        classroom_id = selected_classroom
+
+        add_student = Student.objects.create(
+            name=name,
+            address=address,
+            percentage=percentage,
+            classroom_id=classroom_id
+        )
+
+    return HttpResponse({'message: created'}, content_type="application/json")
+
+
+def update_student(request, id):
+    student = Student.objects.get(id=id)
+    data = json.loads(request.body.decode("utf-8"))
+    selected_classroom = Classroom.objects.get(id=data['classroom_id'])
+    student.name = data['name']
+    student.address = data['address']
+    student.percentage = data['percentage']
+    student.classroom_id = selected_classroom
+
+    student.save()
+
+    return HttpResponse({'message: updated'}, content_type="application/json")
+
+
+def delete_student(request, id):
+    if request.method == 'DELETE':
+        student = Student.objects.get(id=id)
+        student.delete()
+
+    return HttpResponse({'message: deleted'}, content_type="application/json")
+
+
 def get_all_students(request):
     student_list = Student.objects.all().select_related('classroom_id')
     data = []
     for student in student_list:
 
         resp = {
+            "student_id": student.id,
             "name": student.name,
             "address": student.address,
             "percentage": student.percentage,
@@ -55,3 +97,42 @@ def get_sutdent_of_low_percentage(request):
         }
 
     return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+def create_classroom(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode("utf-8"))
+        classroom_name = data['classroom_name']
+        add_classroom = Classroom.objects.create(
+            classroom_name=classroom_name
+        )
+
+    return HttpResponse({'message: created'}, content_type="application/json")
+
+
+def get_all_classroom(request):
+    if request.method == 'GET':
+        data = Classroom.objects.all()
+        resp = [
+        ]
+        for item in data:
+            obj = {
+                "classrrom_id": item.id,
+                "classroom_name": item.classroom_name
+            }
+
+            resp.append(obj)
+
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+def update_classroom(request, id):
+    if request.method == 'POST':
+        data = Classroom.objects.get(id=id)
+        res = json.loads(request.body.decode("utf-8"))
+
+        data.classroom_name = res["classroom_name"]
+
+        data.save()
+
+    return HttpResponse({'message: updated'}, content_type="application/json")
